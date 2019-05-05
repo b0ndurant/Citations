5faf032eef0bc0b524d7a1d9c29c2d82a12cd15a
b0ndurant <martin.aurelien01@gmail.com>
2019-05-05 18:48:44 +0200

    create function randomColor and nextQuote

--- src/Components/Citations.js
+++ src/Components/Citations.js
@@ -1,55 +1,72 @@
 import React, { Component } from 'react';
+import './citations.css';
 import axios from 'axios';
 
 class Citations extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			apiResult: '',
 			author: '',
 			citation: '',
 			episode: '',
-			arrayLength: '',
+			colors: ['blue','red','blueLight','green','gray'],
+			color: '',
 		}
 	}
 
 	componentDidMount() {
-		axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://kaamelott.chaudie.re/api/all/personnage/Arthur`)
+		this.nextQuote()
+	}
+
+	randomColor = () => {
+		let colors = this.state.colors;
+		this.setState({
+			color: colors[Math.floor(Math.random()*colors.length)],
+		})
+	}
+
+	nextQuote = () => {
+		axios.get(`${'https://cors-anywhere.herokuapp.com/'}https://kaamelott.chaudie.re/api/random/personnage/Arthur`)
 
 		.then(response => {
 			this.setState({
-				apiResult: response.data,
-				author: response.data.citation[0].infos.personnage,
-				citation: response.data.citation[0].citation,
-				episode: response.data.citation[0].infos.episode,
-				arrayLength: response.data.citation.length,
+				author: response.data.citation.infos.personnage,
+				citation: response.data.citation.citation,
+				episode: response.data.citation.infos.episode,
 			})
 		})
 		.catch(error => {
 			console.log(error);
 		});
-	}
-
-	random = () => {
-		let min = Math.ceil(0);
-		let max = Math.floor(this.state.arrayLength);
-		let index = Math.floor(Math.random() * (max - min)) + min;
-
-		this.setState({
-			author: this.state.apiResult.citation[index].infos.personnage,
-			citation: this.state.apiResult.citation[index].citation,
-			episode: this.state.apiResult.citation[index].infos.episode,
-		})
-		console.log(this.state.apiResult.citation[1].citation)
+		this.randomColor();
 	}
 
 	render() {
 		return (
 			<div>
-			<div>personnage : { this.state.author }</div>
-			<div>citation : { this.state.citation }</div>
-			<div>episode : { this.state.episode }</div>
-			<button onClick={this.random}>click</button>
+			<div className="columns">
+			<div className="column">
+			<h1 className="title is-1 has-text-centered">Random Citations</h1>
+			</div>
+			</div>
+			<div className="columns is-centered">
+			<div className={"column is-10 " + this.state.color}>
+			<div className="columns">
+			<h4 className="column is-size-4 has-text-weight-bold">episode : { this.state.episode }</h4>
+			</div>
+			<div className="columns">
+			<div className="column has-text-centered is-size-5 is-capitalized is-italic">citation : { this.state.citation }</div>
+			</div>
+			<div className="columns">
+			<h4 className="column has-text-right is-size-4 has-text-weight-bold">" { this.state.author } "</h4>
+			</div>
+			</div>
+			</div>
+			<div className="columns is-centered">
+			<div className="column is-1">
+			<button className="button is-success is-hovered is-medium" onClick={this.nextQuote}>Next Quote</button>
+			</div>
+			</div>
 			</div>
 			)
 	}
