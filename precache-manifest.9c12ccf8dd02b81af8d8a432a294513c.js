0dc65e4a9f87288056a2de7ea0cf6d9fbe6a71aa
b0ndurant <martin.aurelien01@gmail.com>
2019-05-06 09:56:40 +0200

    Updates

---
+++ precache-manifest.9c12ccf8dd02b81af8d8a432a294513c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51fbbee5fe375fc1a86a145766aee2f6",
+    "url": "/citations/index.html"
+  },
+  {
+    "revision": "2875e97f1e261c09e416",
+    "url": "/citations/static/css/main.856e9116.chunk.css"
+  },
+  {
+    "revision": "87e913273ec8e6e3b27a",
+    "url": "/citations/static/js/2.fa9dac84.chunk.js"
+  },
+  {
+    "revision": "2875e97f1e261c09e416",
+    "url": "/citations/static/js/main.e5f286dd.chunk.js"
+  },
+  {
+    "revision": "541539a70768fe677caf",
+    "url": "/citations/static/js/runtime~main.135bfed4.js"
+  }
+]);
